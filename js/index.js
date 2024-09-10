c7f28fdfd0da1613c21f3a4b36cc459be840578b
Felifelps <felipefelipe23456@gmail.com>
2024-09-10 12:10:09 -0300

    Fixes

--- js/index.js
+++ js/index.js
@@ -1,21 +1,5 @@
-// JavaScript para controlar o slider
-const cardsContainer = document.querySelector('.cards-container');
-const prevBtn = document.querySelector('.prev-btn');
-const nextBtn = document.querySelector('.next-btn');
-
-let currentIndex = 0;
-
-prevBtn.addEventListener('click', () => {
-    console.log('clickde')
-    if (currentIndex > 0) {
-        currentIndex--;
-        cardsContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
-    }
-});
-
-nextBtn.addEventListener('click', () => {
-    if (currentIndex < cardsContainer.children.length - 1) {
-        currentIndex++;
-        cardsContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
-    }
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    const menu = document.querySelector('.header-menu');
+    menu.classList.toggle('open');
+    menu.classList.toggle('closed');
 });
