a85ced67fbcc428ce8032fdbd32e8e8e4b06a73c
Felifelps <felipefelipe23456@gmail.com>
2024-09-10 13:51:07 -0300

    Fixes

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
-document.querySelector('.menu-toggle').addEventListener('click', function() {
-    const menu = document.querySelector('.header-menu');
-    menu.classList.toggle('open');
-    menu.classList.toggle('closed');
-});
+const objectiveCards = document.getElementsByClassName('cards-container')[0];
+
+for (let card of objectiveCards.children) {
+    card.addEventListener('click', () => {
+        console.log('clicked')
+    })
+}
