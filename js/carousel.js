89d4e79f0e6afd2e2d8d58faa10403b9afbedca5
Felifelps <felipefelipe23456@gmail.com>
2024-09-12 09:22:53 -0300

    Fixes

--- js/carousel.js
+++ js/carousel.js
@@ -2,7 +2,7 @@ const image = document.getElementsByClassName('carousel-img')[0];
 
 var current = 1;
 
-function changeImage() {
+setInterval(() => {
     image.style.opacity = 0;
     setTimeout(() => {
         image.src = `./images/carrossel-${current}.png`;
@@ -10,6 +10,4 @@ function changeImage() {
         image.style.opacity = 1;
     }, 1700) // 1.7 seconds
     current = current === 3 ? 1 : current + 1;
-}
-
-setInterval(changeImage, 10000); // 10 seconds
+}, 10000); // 10 seconds
